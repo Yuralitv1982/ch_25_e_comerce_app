93771f004e8dc85843e33cff6a715817ba106835
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-08 07:18:21 +0300

    start new chapter 27 custom database

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
-import express from 'express';
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-   res.send(`
-      <div>
-         <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <input name="passwordConfirmation" placeholder="password confirmation" />
-            <button>Sing Up</button>
-         </form>
-      </div>
-      `);
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
 });
 
-const bodyParser = (req, res, next) => {
-   if (req.method === 'POST') {
-      req.on('data', (data) => {
-         const parsed = data.toString('utf8').split('&');
-
-         const formData = {};
-
-         for (let pair of parsed) {
-            const [key, value] = pair.split('=');
-            formData[key] = value;
-         }
-         req.body = formData;
-         next();
-      });
-   } else {
-      next();
-   }
-};
-
-app.post('/', bodyParser, (req, res) => {
-   // get access to password email passwordConfirmation
-   console.log(req);
-   res.send('Account create');
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
-   console.log('listening');
+  console.log('Listening');
 });
