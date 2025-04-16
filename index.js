e3b66da331bcd177e669856a4eb4ac5d7e6aab7f
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-16 19:53:12 +0300

    creating user records and add cookie-session

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.post('/', async (req, res) => {
    if (password !== passwordConfirmation) {
       return res.send('Passwords must match');
    }
+
+   // create a user in our user repo to represent this person
+   const user = await usersRepo.create({ email, password });
+
+   // store the id of that user inside the users cookie
+
    res.send('Account created!!!');
 });
 
