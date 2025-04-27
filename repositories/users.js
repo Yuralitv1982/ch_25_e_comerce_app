b4f6971b443ada9decd52bf4f32d9633810ed326
Yuralitv1982 <yuralitv02@gmail.com>
2025-04-27 14:32:43 +0300

    salting and hashing password

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import crypto from 'crypto';
+import util from 'util';
 
+const scrypt = util.promisify(crypto.scrypt);
 class UsersRepository {
    constructor(filename) {
       if (!filename) {
@@ -24,14 +26,22 @@ class UsersRepository {
    }
 
    async create(attrs) {
+      // attrs === { email: , password: ''}
       attrs.id = this.randomId();
 
+      const salt = crypto.randomBytes(8).toString('hex');
+      const buf = await scrypt(attrs.password, salt, 64);
+
       const records = await this.getAll();
-      records.push(attrs);
+      const record = {
+         ...attrs,
+         password: `${buf.toString('hex')}.${salt}`,
+      };
+      records.push(record);
 
       await this.writeAll(records);
 
-      return attrs;
+      return record;
    }
 
    async writeAll(records) {
